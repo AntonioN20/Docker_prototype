bfa4f0fcdebb056395608239e87803f9cc0fd678
Antonio <aneto2024@jpkeefehs.org>
2023-12-20 12:21:08 -0500

    reset

--- db/testdump.sql
+++
@@ -1,3 +0,0 @@
-CREATE TABLE IF NOT EXISTS users(id int AUTO_INCREMENT, displayName VARCHAR(100), email VARCHAR(320), pic VARCHAR(100), PRIMARY KEY (id));
-ALTER USER 'root' IDENTIFIED WITH mysql_native_password BY 'pass'; 
-flush privileges;
